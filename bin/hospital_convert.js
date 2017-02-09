c28f71319386ee1d6634e89c3f9773ad87f14e49
win10 <win10@DESKTOP-OIU1QEN>
2017-02-09 16:03:35 +0900

    대구광역시 병원목록

--- bin/hospital_convert.js
+++ bin/hospital_convert.js
@@ -17,9 +17,17 @@ parse(f1to4, {comment:"#"}, function(csv_err, csv_data){
    
   for(var cd=1; cd< csv_data.length ; cd++){
     var dataIn = csv_data[cd];
+    
+//    console.log(dataIn[1]);
+    
+    var ldateTemp = dataIn[6].split('-');
+//    console.log(ldateTemp[0]);
+    
+    var ldate = new Date(ldateTemp[0],Number(ldateTemp[1])-1,Number(ldateTemp[2])+1);
+//    console.log(ldate);
       
       
-        if(dataIn[cd] !== undefined){
+        if(dataIn !== undefined){
         var logs = {
         	"number" : dataIn[0],
         	"classification" : dataIn[1],
@@ -27,11 +35,12 @@ parse(f1to4, {comment:"#"}, function(csv_err, csv_data){
         	"domicile_1" : dataIn[3],
         	"domicile_2" : dataIn[4],
         	"tel" : dataIn[5],
-        	"reference_data" : dataIn[6]
+        	"time_slot" : ldate
         	
         }
         
       }
+        console.log(logs);
         
         var fileName = "n_" + dataIn[0] + ".log";
         var logdata = JSON.stringify(logs)+"\n";
